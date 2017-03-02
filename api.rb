cf4fc5e2f76b94ed0e9eebc18e05937bd5e6826e
Brandon Presley <presley.brandon@gmail.com>
2017-03-01 16:14:33 -0800

    Allow POST feature

--- api.rb
+++ api.rb
@@ -3,15 +3,31 @@ require 'sinatra'
 require 'sqlite3'
 
 db = SQLite3::Database.new('blog.db')
-
-get '/' do
-  File.open('README.md')
-end
+db.execute "CREATE TABLE IF NOT EXISTS posts(post_id INTEGER PRIMARY KEY ASC, title TEXT, body TEXT)"
 
 get '/posts' do
-  arr = []
-  db.execute('SELECT * FROM posts') do |row|
-    arr += row
+  posts = db.execute('SELECT * FROM posts;')
+  posts.to_json
+end
+
+post '/post' do
+  begin
+    data = JSON.parse(request.body.read)
+  rescue JSON::Exception => e
+    puts "Exception occured"
+    puts e
   end
-  arr.to_json
+  post_id = db.get_first_value('SELECT count(*) FROM posts') + 1 
+  begin
+    db.transaction
+    db.execute(
+      "INSERT INTO posts (post_id, title, body) " +
+      "VALUES (#{post_id}, #{data['title'].to_s}, #{data['body'].to_s})"
+    )
+    db.commit
+  rescue SQLite3::Exception => e 
+    puts "Exception occurred"
+    puts e
+    db.rollback
+  end
 end
